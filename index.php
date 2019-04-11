54b8460efa3479fc0a8a177f498eab6384a5bd14
Miklós Ákos <m.akos.kms@gmail.com>
2019-04-11 15:47:01 +0200

    Frissítés
    yey

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
-	include("./engine/config.php") ;
+	include("./engine/config.php");
+	$url="";
+	switch($channel){
+		case "dev": $url=$channel; break;
+		case "rel": $url="master";break;
+		default: $url="dev";break;
+	}
 ?>
-<html>
+<html lang="hu">
 	<head>
 		<title>akos_tech</title>
 		<link rel="stylesheet" type="text/css" href="./engine/style.css">
@@ -17,24 +23,29 @@
 	<body>
 			<script src="./engine/aboutshowhide.js"></script>
 			<div id="progman">
-				<div id="progmanheader">Programkezelő</div>
+				<div id="progmanheader">Programkezelő </div>
 					<div id="progmanmenu">
-						<button onclick="openhelp()" class="dropbtn">Névjegy</button>
+						<button onclick="openhelp()" class="dropbtn">Névjegy</button> <button class="dropbtn" onclick="openchangelog()" class="dropbtn2" id="changelogbtn">Változások listája</button>
 					</div>
-				<a class="link" id="yt" href="https://www.youtube.com/channel/UCXGXHJug86ym9ik0iPiycrQ"></a> <a class="link" id="fb" href="#"></a> <a class="link" id="ig" href="https://instagram.com/miklos.akos99"></a> <a class="link" id="github" href="https://github.com/miklosakos/WebWinUI/"></a>
+				<a class="link" id="yt" href="https://www.youtube.com/channel/UCXGXHJug86ym9ik0iPiycrQ"></a> <a class="link" id="fb" href="#"></a> <a class="link" id="ig" href="https://instagram.com/miklos.akos99"></a> <a class="link" id="github" href="https://github.com/miklosakos/WebWinUI/tree/<?php echo $url;?>"></a> <a class="link" id="twitter" href="https://twitter.com/miklos_akos"></a> <a class="link" id="twitter" href="https://twitter.com/56kbpswebsite"></a>
 			</div>
 			<div id="about" class="dropdown-content">
-							<div id="aboutheader"> <button id="close" onclick="openhelp()" class="dropbtn">-</button>A WebWinUI névjegye</button></div>
+							<div id="aboutheader"> <button id="close" onclick="openhelp()" class="dropbtn">-</button>A WebWinUI névjegye</div>
 						<img id="logo" src="https://via.placeholder.com/128x128">
 						<p id="main">WebWinUI<br/>
 						Verzió: <?php echo $ver."(".$builddate.")";?>, <?php echo $channel;?> csatorna.<br/>
-						Copyright &copy; 2018 miklos_akos</p>
-						<button onclick="openhelp()" class="dropbtn" id="ok">OK</button>						<p id="about1">A WebWinUI megpróbálja újraörökíteni a régi Windows 3-as felületet, valamint a <a href="https://miklosakos.eu">weboldalam</a> alapja.</p>
+						Copyright &copy; 2018-2019 miklos_akos</p>
+						<button onclick="openhelp()" class="dropbtn" id="ok">OK</button>						<p id="about1">A WebWinUI megpróbálja újraörökíteni a régi Windows 3-as felületet, valamint a <a href="https://miklosakos.eu">weboldalam</a> alapja. </p>
 						<p id="about2">Ha szeretnéd te is használni ezt az oldaladon, akkor kattints a GitHub linkre a programkezelőben, vagy <a href="#">ide</a>.</p>
 						<p id="about3">Figyelem! Az itt felhasznált szellemi anyagok azon tulajdonosaik szellemi tulajdonát képezik!</p>
-						</div>
+			</div>
+			<div id="changelog" class="dropdown-content">
+				<div id="changelogheader"> <button id="close" onclick="openchangelog()" class="dropbtn">-</button>Változások listája</div>
+				<?php include("./engine/changelog.php");?>
+			</div>
 			<script src="./engine/script.js"></script>
 			<script src="./engine/script2.js"></script>
-			<script src="./engine/aboutmv.js"></script>
+			<script src="./engine/script3.js"></script>
+			<script src="./engine/clock.js"></script>
 	</body>
 </html>
