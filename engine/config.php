97d1078affeb03750fc81e9e7b387444734b35cb
Miklós Ákos <m.akos.kms@gmail.com>
2019-10-20 20:19:31 +0200

    Delete config.php

--- engine/config.php
+++
@@ -1,15 +0,0 @@
-<?php
-/*$dbhost="localhost";
-$dbusr="root";
-$dbpw="";
-$db="webos";
-*/$ver="1.0";
-$lang="hu";
-$builddate="2019-03-28";$channel="dev";
-/*$conn=mysqli_connect($localhost,$dbusr,$dbpw,$db);
-if($conn){
-}else{
-	die("Nem sikerült csatlakozni az adatabázishoz!");
-}
-mysqli_close($conn);
-*/?>
